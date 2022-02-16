fa7492a669ffab30766955a07914e81eb024fa5a
macbook-air <techcom.ec20@gmail.com>
2022-02-17 03:12:38 +0700

    add: ex testing

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -8,5 +8,26 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        //Arrange
+        var expect = 123;
+
+        //Act
+        var actual = 123;
+
+        //Assertå
+        Assert.AreEqual(expect, actual);
+    }
+
+    [TestMethod]
+    public void Test2()
+    {
+        //Arrange
+        var expect = 123;
+
+        //Act
+        var actual = 222;
+
+        //Assert
+        Assert.AreNotEqual(expect, actual);
     }
 }
