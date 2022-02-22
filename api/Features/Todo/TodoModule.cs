cf1c8341af8ae8a662f4ab99c70f7b69a1912170
macbook-air <techcom.ec20@gmail.com>
2022-02-22 15:00:52 +0700

    add api todos

--- api/Features/Todo/TodoModule.cs
+++ api/Features/Todo/TodoModule.cs
@@ -1,16 +1,17 @@
 using Carter;
 
-namespace Api.Features.Todo
+namespace Api.Features.Todo;
+public class TodoModule : ICarterModule
 {
-    public class TodoModule : ICarterModule
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
-        public void AddRoutes(IEndpointRouteBuilder app)
+        app.MapGet("/api/todos", () =>
         {
-            app.MapGet("/api/todos", () =>
-            {
-                return "OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
-            });
-
-        }
+            var response = new List<TodoModel> {
+                new TodoModel{ Id= 1, Title= "Todo 1", IsActive = true},
+                new TodoModel{ Id= 2, Title= "Todo 2", IsActive = true},
+            };
+            return Results.Ok(response);
+        });
     }
 }
