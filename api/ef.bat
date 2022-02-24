6feaedda30dde280657a1df0fd8132d07ecf20b2
jutipong.su <techcom.ec20@gmail.com>
2022-02-24 21:32:01 +0700

    add scaffold

--- api/ef.bat
+++ api/ef.bat
@@ -1,8 +1,19 @@
 set host=localhost 
-set dbname=Test-Test
-set output_dir=Entities\Model
-set context_dir=Entities\DbContext 
-set context_name=AppContext
+set port=1433
+set timeout=30
+set dbname=demo1
+set user=sa
+set password=p@ssw0rd
+set context_name=DbContexts
+set context_dir=.\Entities 
+set output_dir=.\Entities\Models
 
-dotnet ef dbcontext scaffold "Server=%host%;Database=%dbname%; Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer --data-annotations --use-database-names --output-dir %output_dir% --context-dir %context_dir% --context "%context_name%" --no-onconfiguring --force
- 
+dotnet ef dbcontext scaffold "Server=%host%,%port%;Initial Catalog=%dbname%;User ID=%user%; Password=%password%; Timeout=%timeout%;" ^
+Microsoft.EntityFrameworkCore.SqlServer ^
+--use-database-names ^
+--data-annotations ^
+--no-onconfiguring ^
+--context %context_name% ^
+--context-dir %context_dir% ^
+--output-dir %output_dir% ^
+--force
