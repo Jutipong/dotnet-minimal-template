28f36c52c29a2793720ad44185b00142eab785cd
Jutipong.Dev <techcom.ec20@gmail.com>
2023-02-21 22:13:02 +0700

    update config serilog

--- api/Extensions/ServiceCollection/Logging.cs
+++ api/Extensions/ServiceCollection/Logging.cs
@@ -7,8 +7,11 @@ public static partial class ServiceCollection
     public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
     {
         builder.Logging.ClearProviders();
-        builder.Host.UseSerilog((context, services, configuration) => configuration
-            .ReadFrom.Configuration(context.Configuration).WriteTo.Console());
+        builder.Host.UseSerilog((context, services, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(context.Configuration);
+            // .ReadFrom.Configuration(context.Configuration).WriteTo.Console()
+        });
         return builder;
     }
 }
